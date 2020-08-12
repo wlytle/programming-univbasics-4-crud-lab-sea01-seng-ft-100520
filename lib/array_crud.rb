3909e24968dbdfa45f5ab189fe86d8cdb11c695f
Will Lytle <wlytle89@gmail.com>
2020-08-12 23:15:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,17 @@
 def create_an_empty_array
-  
+  array = []
 end
 
 def create_an_array
-  
+  array = Array.new(4)
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
